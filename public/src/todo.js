f1571bd6434b0e7d4b2e9763bee1dd62d0c9ba90
emi-smith <87035540+emi-smith@users.noreply.github.com>
2023-05-10 21:48:20 -0600

    Updated todo JS

--- public/src/todo.js
+++ public/src/todo.js
@@ -1,38 +1,224 @@
-// Add a new task
-const form = document.getElementById("to-do-form");
-const input = document.getElementById("to-do-input");
-const toDoDecks = document.getElementById("to-do");
+    const todoInput = document.querySelector(".todo-input");
+    const todoButton = document.querySelector(".todo-button");
+    const taskWrap = document.getElementById("task-wrap");
+    const filterOption = document.querySelector(".filter-todo");
 
-form.addEventListener("submit", (e) => {
-    e.preventDefault();
-    const value = input.value;
+    document.addEventListener("DOMContentLoaded", getLocalTodos);
+    todoButton.addEventListener("click", addTodo);
+    taskWrap.addEventListener("click", deleteCheck);
+    filterOption.addEventListener("change", filterTodo);
 
-    if (!value) return;
+    function addTodo(event) {
+    event.preventDefault();
 
-    const newTask = document.createElement("p");
-    newTask.classList.add("tasks");
-    newTask.setAttribute("draggable", "true");
-    newTask.innerText = value;
-
-    // Set new task to drag and drop
-    newTask.addEventListener("dragstart", () => {
-        newTask.classList.add("is-dragging");
-    });
-
-        newTask.addEventListener("dragend", () => {
-        newTask.classList.remove("is-dragging");
-    });
-
-// clear input fields
-    toDoDecks.appendChild(newTask);
-
-    input.value = ""
-});
-
-// Get tasks from local storage 
-window.addEventListener('load', () => {
-    tasks = JSON.parse(localStorage.getItem('tasks')) || [];
-});
+        // CREATE TODO DIV
+        const todoDiv = document.createElement("div");
+        todoDiv.classList.add("todo");
+        todoDiv.setAttribute("draggable", "true");
 
 
-// Add task to deck selected with radio input
+        // Set new task to drag and drop
+        todoDiv.addEventListener("dragstart", () => {
+        todoDiv.classList.add("is-dragging");
+        });
+
+        todoDiv.addEventListener("dragend", () => {
+        todoDiv.classList.remove("is-dragging");
+        });
+
+                // CREATE NEW TODO INPUT
+                const newTodo = document.createElement("input");
+                // newTodo.innerText = todoInput.value; 
+                newTodo.setAttribute("value", todoInput.value);
+                newTodo.classList.add("todo-item");
+                newTodo.setAttribute("type", "text");
+                newTodo.setAttribute("name", "content");
+                newTodo.setAttribute("readonly", "readonly");
+                newTodo.setAttribute("placeholder","Text will go here");
+
+                todoDiv.appendChild(newTodo);
+
+                //ADDING TO LOCAL STORAGE 
+                saveLocalTodos(todoInput.value);
+            
+            // CREATE ACTIONS CONTAINER
+            const actionsContainer = document.createElement("div");
+            actionsContainer.classList.add("actions-container");
+            todoDiv.appendChild(actionsContainer)
+            
+                // CREATE FINISH TASK LABEL
+                const finishedTask = document.createElement("label"); 
+                finishedTask.classList.add("finished-task");
+                actionsContainer.appendChild(finishedTask);
+
+                    // CREATE CHECKBOX COMPLETE BUTTON
+                    const completedButton = document.createElement("input");
+                    completedButton.classList.add("complete-btn");
+                    completedButton.setAttribute("type", "checkbox");
+                    finishedTask.appendChild(completedButton);
+
+                // CREATE ACTIONS DIV
+                const actionsDiv = document.createElement("div");
+                actionsDiv.classList.add("actions");
+                actionsContainer.appendChild(actionsDiv);
+
+                    // CREATE EDIT BUTTON
+                    const editButton = document.createElement("button");
+                    editButton.innerHTML = 'Edit';
+                    editButton.classList.add("edit");
+                    actionsDiv.appendChild(editButton);
+
+                    // CREATE DELETE BUTTON
+                    const trashButton = document.createElement("button");
+                    trashButton.innerHTML = 'Delete';
+                    trashButton.classList.add("delete");
+                    actionsDiv.appendChild(trashButton);
+
+                    // APPEND TASK TO TASK CONTAINER
+                    taskWrap.appendChild(todoDiv);
+                    todoInput.value = "";
+
+
+
+}
+
+    function deleteCheck(e) {
+        const item = e.target;
+
+        if(item.classList[0] === "delete") {
+            const todo = item.parentElement;
+            todo.classList.add("slide");
+
+            removeLocalTodos(todo);
+            todo.addEventListener("transitionend", function() {
+                todo.remove();
+            });
+        }
+
+        if(item.classList[0] === "complete-btn") {
+            const todo = item.parentElement;
+            todo.classList.toggle("completed");
+        }
+    }
+
+    function filterTodo(e) {
+        const todos = taskWrap.childNodes;
+        todos.forEach(function(todo) {
+            switch(e.target.value) {
+                case "all": 
+                    todo.style.display = "flex";
+                    break;
+                case "completed": 
+                    if(todo.classList.contains("completed")) {
+                        todo.style.display = "flex";
+                    } else {
+                        todo.style.display = "none";
+                    }
+                    break;
+                case "incomplete":
+                    if(!todo.classList.contains("completed")) {
+                        todo.style.display = "flex";
+                    } else {
+                        todo.style.display = "none";
+                    }
+                    break;
+            }
+        });
+    }
+
+    function saveLocalTodos(todo) {
+        let todos;
+        if(localStorage.getItem("todos") === null) {
+            todos = [];
+        } else {
+            todos = JSON.parse(localStorage.getItem("todos"));
+        }
+        todos.push(todo);
+        localStorage.setItem("todos", JSON.stringify(todos));
+    }
+
+    function getLocalTodos() {
+        let todos;
+        if(localStorage.getItem("todos") === null) {
+            todos = [];
+        } else {
+            todos = JSON.parse(localStorage.getItem("todos"));
+        }
+        
+        todos.forEach(function(todo) {
+            // CREATE TODO DIV
+            const todoDiv = document.createElement("div");
+            todoDiv.classList.add("todo");
+            todoDiv.setAttribute("draggable", "true");
+            // Set new task to drag and drop
+            todoDiv.addEventListener("dragstart", () => {
+            todoDiv.classList.add("is-dragging");
+            });
+            todoDiv.addEventListener("dragend", () => {
+            todoDiv.classList.remove("is-dragging");
+            });
+
+                    // CREATE NEW TODO INPUT
+                    const newTodo = document.createElement("input");
+                    // newTodo.innerText = todoInput.value; 
+                    newTodo.setAttribute("value", todoInput.value);
+                    newTodo.classList.add("todo-item");
+                    newTodo.setAttribute("type", "text");
+                    newTodo.setAttribute("name", "content");
+                    newTodo.setAttribute("readonly", "readonly");
+                    newTodo.setAttribute("placeholder","Text will go here");
+                    todoDiv.appendChild(newTodo);
+
+                
+                // CREATE ACTIONS CONTAINER
+                const actionsContainer = document.createElement("div");
+                actionsContainer.classList.add("actions-container");
+                todoDiv.appendChild(actionsContainer)
+                
+                    // CREATE FINISH TASK LABEL
+                    const finishedTask = document.createElement("label"); 
+                    finishedTask.classList.add("finished-task");
+                    actionsContainer.appendChild(finishedTask);
+
+                        // CREATE CHECKBOX COMPLETE BUTTON
+                        const completedButton = document.createElement("input");
+                        completedButton.classList.add("complete-btn");
+                        completedButton.setAttribute("type", "checkbox");
+                        finishedTask.appendChild(completedButton);
+
+                    // CREATE ACTIONS DIV
+                    const actionsDiv = document.createElement("div");
+                    actionsDiv.classList.add("actions");
+                    actionsContainer.appendChild(actionsDiv);
+
+                        // CREATE EDIT BUTTON
+                        const editButton = document.createElement("button");
+                        editButton.innerHTML = 'Edit';
+                        editButton.classList.add("edit");
+                        actionsDiv.appendChild(editButton);
+
+                        // CREATE DELETE BUTTON
+                        const trashButton = document.createElement("button");
+                        trashButton.innerHTML = 'Delete';
+                        trashButton.classList.add("delete");
+                        actionsDiv.appendChild(trashButton);
+
+                        // APPEND TASK TO TASK CONTAINER
+                        taskWrap.appendChild(todoDiv);
+                        todoInput.value = "";
+                    
+                });
+    }
+
+    function removeLocalTodos(todo) {
+        let todos;
+        if(localStorage.getItem("todos") === null) {
+            todos = [];
+        } else {
+            todos = JSON.parse(localStorage.getItem("todos"));
+        }
+
+        const todoIndex = todo.children[0].innerText;
+        todos.splice(todos.indexOf(todoIndex), 1);
+        localStorage.setItem("todos", JSON.stringify(todos));
+    }
